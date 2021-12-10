4b7cccc13328543164c0daedce8fad47833cf4fb
praispranav <praispranav@gmail.com>
2021-12-10 21:29:51 +0530

    Added Delete To Cart

--- routes/order.js
+++ routes/order.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const Cart = require("../schema/cart");
 const config = require("../config/keys");
 const Product = require("../schema/categories");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const privateKey = config.privateKey;
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
 const tokenVerify = (token) => {
   return new Promise((resolve, rejects) => {
@@ -30,22 +30,22 @@ router.post("/cart/get", async (req, res) => {
         userId: user.data._id,
       });
       const productIds = new Array();
-      result.forEach((item)=>{
-        productIds.push(mongoose.Types.ObjectId(item.productId))
-      })
+      result.forEach((item) => {
+        productIds.push(mongoose.Types.ObjectId(item.productId));
+      });
       const getRequiredProducts = await Product.find({
         _id: { $in: productIds },
       });
       const productById = new Array();
-      getRequiredProducts.forEach((item)=> productById[item._id] = item)
+      getRequiredProducts.forEach((item) => (productById[item._id] = item));
 
-      const cartWithProduct = result.map((item)=>{
-        const product = productById[item.productId]
-        const obj = { 
+      const cartWithProduct = result.map((item) => {
+        const product = productById[item.productId];
+        const obj = {
           _id: item._id,
           productId: product._id,
-          userId:item.userId,
-          selectedQuantity: item.selectedQuantity, 
+          userId: item.userId,
+          selectedQuantity: item.selectedQuantity,
           name: product.name,
           availableQuantity: product.availableQuantity,
           initialQuantity: product.initialQuantity,
@@ -55,62 +55,25 @@ router.post("/cart/get", async (req, res) => {
           discount: product.discount,
           category: product.category,
           status: product.status,
-          description: product.description
-        } 
-        return obj
-      })
-      let totalPrice = 0
-      cartWithProduct.forEach((item)=>{
-        const localTotalPrice = item.price * item.selectedQuantity
-        totalPrice += localTotalPrice 
-      })
-      
+          description: product.description,
+        };
+        return obj;
+      });
+      let totalPrice = 0;
+      cartWithProduct.forEach((item) => {
+        const localTotalPrice = item.price * item.selectedQuantity;
+        totalPrice += localTotalPrice;
+      });
+
       res.status(200).json({ data: cartWithProduct, total: totalPrice });
     } else {
-        res.json({ message: "Cart Empty" });
+      res.json({ message: "Cart Empty" });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ message: "Unauthorised User" });
   }
 });
-
-// router.post("/cart/total", async ()=>{
-//   const { token } = req.body
-//   try{
-//     const user = await tokenVerify(token);
-//     if (user.data._id) {
-//       const result = await Cart.find({
-//         userId: user.data._id,
-//       });
-//       const productIds = new Array();
-//       result.forEach((item)=>{
-//         productIds.push(mongoose.Types.ObjectId(item.productId))
-//       })
-//       const getRequiredProducts = await Product.find({
-//         _id: { $in: productIds },
-//       });
-//       const productById = new Array();
-//       getRequiredProducts.forEach((item)=> productById[item._id] = item)
-
-//       const cartWithProduct = result.map((item)=>{
-//         const product = productById[item.productId]
-//         const obj = { 
-//           selectedQuantity: item.selectedQuantity, 
-          
-//           price: product.price,
-//           priceUnit: product.priceUnit,
-//         } 
-//         return obj
-//       })
-
-//       let total = 
-//     }
-//   } catch(error){
-//       res.status(500).json({ message: "Unauthorised User" });
-//      }
-//   }
-// })
 
 router.post("/cart/add", async (req, res) => {
   const { token, productId, selectedQuantity } = req.body;
@@ -124,10 +87,10 @@ router.post("/cart/add", async (req, res) => {
       });
       res.status(201).json({ message: "Item Added TO Cart" });
     } else {
-        res.status(401).json({ message: "Item Cannot be Added" });
+      res.status(401).json({ message: "Item Cannot be Added" });
     }
   } catch (error) {
-      console.log(error)
+    console.log(error);
     res.status(500).json({ message: "Unauthorised User" });
   }
 });
@@ -142,17 +105,17 @@ router.post("/cart/edit/", async (req, res) => {
         {
           selectedQuantity: selectedQuantity,
         }
-      ); 
+      );
       res.status(201).json({ message: "Item Updated" });
     } else {
-        res.json({ message: "Item Cannot be Added" });
+      res.json({ message: "Item Cannot be Added" });
     }
   } catch (error) {
     res.status(500).json({ message: "Unauthorised User" });
   }
 });
 
-router.post("/cart/delete", async () => {
+router.post("/cart/delete", async (req, res) => {
   const { token, _id } = req.body;
   try {
     const user = await tokenVerify(token);
@@ -160,13 +123,48 @@ router.post("/cart/delete", async () => {
       await Cart.deleteOne({ userId: user.data._id, _id: _id });
       res.status(201).json({ message: "Item Deleted" });
     } else {
-        res.json({ message: "Item Cannot be Added" });
+      res.json({ message: "Item Cannot be Added" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Unauthorised User" });
+  }
+});
+var NormalOrder = require("../schema/normalOrders");
+
+router.post("/add", async (req, res) => {
+  const { token, orders: originalOrders } = req.body;
+  try {
+    const user = await tokenVerify(token);
+    const userId = user.date._id;
+    const orders = originalOrders.map((item) => {
+      item.userId = userId;
+      item.status = "Processing";
+      item.createdDate = new Date();
+      return item;
+    });
+    const result = await NormalOrder.insertMany([...orders]);
+    if (result) {
+    } else {
+      res.status(400).json({ message: "Order Failed" });
     }
   } catch (error) {
     res.status(500).json({ message: "Unauthorised User" });
   }
 });
 
-
+router.post("/edit", async (req, res) => {
+  const { token, _id } = req.body;
+  try {
+    const user = await tokenVerify(token);
+    if (user.data._id) {
+      await Cart.deleteOne({ userId: user.data._id, _id: _id });
+      res.status(201).json({ message: "Item Deleted" });
+    } else {
+      res.json({ message: "Item Cannot be Added" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Unauthorised User" });
+  }
+});
 
 module.exports = router;
---
+++ schema/normalOrders.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+const Orders = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    productId: { type: String, required: true },
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    priceUnit: { type: String, required: true },
+    selectedQuantity: { type: String, required: true },
+    status: { type: String, required: true },
+    createdDate: { type: Date, required: true },
+    addressId: { type: String, required: true },
+    address1: { type: String, required: true },
+    address2: { type: String, required: true },
+    pinCode: { type: String, required: true },
+    phone: { type: String, required: true },
+    paymentMode: { type: String, required: true },
+  },
+  { collection: "normal-order" }
+);
+
+module.exports = mongoose.model("Normal Order", Orders);
